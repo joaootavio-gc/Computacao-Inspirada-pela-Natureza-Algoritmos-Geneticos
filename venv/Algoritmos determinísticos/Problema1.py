23c01e8d6779ef64d3be05fce7f1183b49edc6e2
João Otávio <joao.otavio@unesp.br>
2018-04-19 10:16:28 -0300

    Função de seleção

--- venv/Algoritmos determinísticos/Problema1.py
+++ venv/Algoritmos determinísticos/Problema1.py
@@ -1,9 +1,11 @@
-import random
+import random, numpy
 
 def Problema1(probabilidadeCrossOver, probabilidadeMutacao):
-    alvo = [1,1,1,1,0,1,1,0,1,1,1,1]
     populacao = inicializar()
     aptidao = avaliar(populacao)
+    if 12 in aptidao:
+        print('Um indivíduo alcançou o alvo!\n')
+
     t = 1
     while True:
         populacao = selecionar(populacao, aptidao)
@@ -25,7 +27,20 @@ def inicializar():
 
 
 def selecionar(populacao, aptidao):
-    return 1
+    somaAptidoes = sum(aptidao)
+    porcaoRoleta = []
+    selecionados = []
+    porcaoRoleta.append(360*aptidao[0]/somaAptidoes)
+    for i in range(1,8):
+        porcaoRoleta.append((360*aptidao[i]/somaAptidoes) + porcaoRoleta[i-1])
+    for i in range(0, 8):
+        sorteado = random.randint(0, 360)
+        somaAuxiliar = 0 #para percorrer as porções da roleta buscando o elemento correto
+        i = 0 #para guardar o indice do individuo escolhido
+        while porcaoRoleta[i] < sorteado:
+            i += 1
+        selecionados.append(populacao[i])
+    return selecionados
 
 def reproduzir(populacao, aptidao, probabilidadeCrossOver):
     return 1
@@ -34,9 +49,12 @@ def variar(populacao, probabilidadeMutacao):
     return 1
 
 def avaliar(populacao):
+    alvo = [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1]
     aptidao = []
     for individuo in populacao:
-        aptidao.append(sum(individuo))
+        #contar o numero de elementos diferentes e subtrair 12 é o mesmo que contar os iguais
+        #usando um vetor numpy, cada elemento de aptidão conterá os elementos iguais
+        aptidao.append(numpy.sum(numpy.array(individuo) == numpy.array(alvo)))
     return aptidao
 
 if __name__ == '__main__':
